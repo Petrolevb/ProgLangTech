a400f5366218a4b37e49315c0524012c31309230
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2013-02-05 17:13:04 +0100

    Implementation suivant le Impl Prog Lang

---
+++ TypeCheck/TypeCheck.hs
@@ -0,0 +1,51 @@
+module TypeCheck where
+
+type Type  = String
+type Ident = String
+type Env = [(Type, Ident)]
+
+
+-- Has to wait an import
+type Exp = String
+type Statement = [String]
+type Def = Bool
+type FunType = Bool
+
+type Program = [Statement]
+
+-- infer type of exp
+infer :: Env -> Exp -> Type
+infer = undefined
+
+-- Check type of exp
+checkExp :: Env -> Exp -> Type -> Maybe String 
+checkExp gamma e t = Nothing
+
+-- check sequence of statetments
+checkStm :: Env -> Statement -> Maybe String
+checkStm gamma stm = Nothing
+
+-- check function definition
+checkFun :: Env -> Def -> Maybe String
+checkFun gamma fun = Nothing
+
+-- Check a whole program
+check :: Program -> Maybe String
+check prog = Nothing
+
+lookupVar :: Ident -> Env -> Type
+lookupVar = undefined
+lookupFun :: Ident -> Env -> FunType
+lookupFun = undefined
+
+extendVar :: Env -> Ident -> Type -> Env
+extendVar = undefined
+extendFun :: Env -> Def -> Env 
+extendFun = undefined
+
+emptyEnv :: Env
+emptyEnv = []
+
+newblock :: Env -> Env
+newblock = undefined
+
