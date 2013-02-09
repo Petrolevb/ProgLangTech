9b860cd94d8a71b24227653c75ac20c851c2dc65
Petrolevb-XIrisProg <Vincebellec@gmail.com>
2013-02-09 14:31:16 +0100

    Pattern Match sur buildEnvFromStms

--- TypeCheck/Context.hs
+++ TypeCheck/Context.hs
@@ -1,37 +1,48 @@
 module Context where
 
+import AbsCPP
+import LexCPP
+import ParCPP
+import ErrM
+
+-- Return the good thing or an error by a string
+
 -- Signature is the name of the function and list of types
-type Signature = (Ident, FunType)
+type Signature = (Id, FunType)
 type FunType   = ([Type], Type)
 
 -- Env is a stack of context and the function initialisation
 type Env     = (Signature, [Context])
-type Context = [(Type, Ident)]
+type Context = [(Type, Id)]
 
 -- Building the context
 -- Has to check if already exist
-extendVar :: Env -> Ident -> Type -> Env
+extendVar :: Env -> Id -> Type -> Env
 extendVar (s, (gamma:stack)) id ty = (s, ((ty, id):gamma):stack)
 extendVar (s, [])            id ty = (s, ((ty, id):[]):[])
 
 extendFun :: Env -> Def -> Env 
 extendFun = undefined
 
-emptyEnv :: Env
-emptyEnv = (("", ([], "")), [])
+emptyEnv :: Signature -> Env
+emptyEnv s = (s, [])
 
 newblock :: Env -> Env
 newblock (s, c) = (s, []:c)
 
 
+funToSign :: Def -> (Signature, [Stm])
+funToSign (DFun typeFun name args stms) = ((name, ((fromArgs args), typeFun)), stms)
+    where fromArgs args = map (\(ADecl ty _) -> ty) args
+
 
 -- get informations from the context
 
-lookupVar :: Ident -> Env -> Error Type
-lookupVar id (_, [])                   = Error "Variable not found"
+lookupVar :: Id -> Env -> Err Type
+lookupVar id (_, [])                   = Bad "Variable not found"
 lookupVar id (s, ([]:stack))           = lookupVar id (s, stack)
 lookupVar id (s, (((t, i):env):stack)) | i == id   = Ok t
                                        | otherwise = lookupVar id (s, (env:stack))
 
-lookupFun :: Ident -> Env -> Error FunType
+lookupFun :: Id -> Env -> Err FunType
 lookupFun = undefined
--- TypeCheck/TypeChecker.hs
+++ TypeCheck/TypeChecker.hs
@@ -6,54 +6,59 @@ import AbsCPP
 import PrintCPP
 import ErrM
 
--- Type from CPP.cf
---type Type  = "int" | "double" | "bool" | "void" 
-type Ident = String
-
--- Return the good thing or an error by a string
-type Error a = Error String | Ok a
-
--- Type Program from CPP.cf
--- Type Exp     from CPP.cf
-
-
-typecheck :: Program -> Err ()
-typecheck p = typeResult $ all $ map checkDef (map buildEnvOnDef p)
-
-typeResult :: Bool -> Err ()
-typeResult = undefined
 {-
 	- Has to work in two passes
 	- the first "build" the database of context
 	- the second check and anotate the code
 -}
+typecheck :: Program -> Err ()
+typecheck p = typeResult $ and $ 
+                [ checkDef env def | 
+                    (env, def) <- zip 
+                                (map buildEnvOnDef (fromProg p)) 
+                                (fromProg p) 
+                ]
+    where fromProg (PDefs defs) = defs
 
--- Program = [Def] 
--- check the Def just after building the environment
+typeResult :: Bool -> Err ()
+typeResult False = Bad "Error on type checking"
+typeResult True  = Ok ()
+
 buildEnvOnDef :: Def -> Env
-buildEnvOnDef def = undefined
+buildEnvOnDef fun = buildEnvFromStatements (emptyEnv funSig) stms
+    where  (funSig, stms) = funToSign fun
+
+buildEnvFromStatements :: Env -> [Stm] -> Env
+buildEnvFromStatements e ((SExp    exp)          :s) = buildEnvFromStatements e s
+buildEnvFromStatements e ((SDecls  typ ids)      :s) = buildEnvFromStatements e s
+buildEnvFromStatements e ((SInit   typ id expVal):s) = buildEnvFromStatements e s
+buildEnvFromStatements e ((SReturn exp)          :s) = buildEnvFromStatements e s
+buildEnvFromStatements e ((SWhile  exp stm)      :s) = buildEnvFromStatements e s
+buildEnvFromStatements e ((SBlock  stms)         :s) = buildEnvFromStatements e s
+buildEnvFromStatements e ((SIfElse exp stmI stmE):s) = buildEnvFromStatements e s
+buildEnvFromStatements e []      = e
 
 checkDef :: Env -> Def -> Bool
 checkDef = undefined
 
 -- infer type of exp
-infer :: Env -> Exp -> Error Type
+infer :: Env -> Exp -> Err Type
 infer = undefined
 
 -- Check type of exp
-checkExp :: Env -> Exp -> Type -> Error ()
+checkExp :: Env -> Exp -> Type -> Err ()
 checkExp gamma e t = Ok ()
 
 -- check sequence of statetments
-checkStm :: Env -> Statement -> Error ()
+checkStm :: Env -> Stm -> Err ()
 checkStm gamma stm = Ok ()
 
 -- check function definition
-checkFun :: Env -> Def -> Error ()
+checkFun :: Env -> Def -> Err ()
 checkFun gamma fun = Ok ()
 
 -- Check a whole program
-check :: Program -> Error ()
+check :: Program -> Err ()
 check prog = Ok ()
 
 
